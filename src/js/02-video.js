8a17684d0208503da466e45a79cf63f3754ee562
Deniss Simonaits <dsimonaits@gmail.com>
2022-09-24 19:27:27 +0300

    Task 2 finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,17 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on(
+  'timeupdate',
+  throttle(data => {
+    console.log(data.seconds);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.setVolume(0.5);
