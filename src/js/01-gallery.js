2a132712f03a6c74dd3ea76844ae7b4ad3a52b3f
Deniss Simonaits <dsimonaits@gmail.com>
2022-09-24 14:50:33 +0300

    LightBox update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,4 @@
-import SimpleLightbox from 'simplelightbox';
-
+import 'simplelightbox/dist/simplelightbox.min.js';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items.js';
@@ -14,42 +13,14 @@ galleryRef.insertAdjacentHTML('afterbegin', createGalleryItem);
 function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `
-        <div class="gallery__item">
-          <a class="gallery__link" data-href="${original}">
-            <img
-              class="gallery__image"
-              src="${preview}"
-              data-source="${original}"
-              alt="${description}"
-            />
-          </a>
-        </div>`;
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
     })
     .join('');
 }
 
-galleryRef.onclick = e => {
-  const galleryItemSource = e.target.dataset.source;
-  const newLightbox = basicLightbox.create(
-    `
-		<img width="1400" height="900" src="${galleryItemSource}">
-	`,
-    {
-      onShow: () => {
-        window.addEventListener('keydown', onEscKeyPress);
-      },
-
-      onClose: () => {
-        window.removeEventListener('keydown', onEscKeyPress);
-      },
-    }
-  );
-  newLightbox.show();
-
-  function onEscKeyPress(e) {
-    if (e.code === 'Escape') {
-      newLightbox.close();
-    }
-  }
-};
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
